4125b7945a0a457043a5d7cb65d77b13c0cd2880
Epicpp <peverelli.t@gmail.com>
2024-09-04 15:50:39 +0200

    add

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,7 +7,7 @@
                 <!-- Logo -->
 				<div class="shrink-0 flex items-center">
 					<a href="{{ route('huilehes.index') }}">
-						<x-application-logo class="block h-9 w-auto fill-current text-green-600" />
+						<x-application-logo class="block h-12 w-auto fill-current text-green-600" />
 					</a>
 				</div>				
                 <!-- Navigation Links -->
