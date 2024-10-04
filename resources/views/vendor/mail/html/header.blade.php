3a8384b357439ccdcc883ca5d787322fa16010ae
Epicpp <peverelli.t@gmail.com>
2024-10-04 21:00:21 +0200

    add banner image

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header" style="background-color: #647a0b; text-align: center;">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('images/png-01.png') }}" class="logo" alt="{{ config('app.name') }}" style="height: 60px;">
+            <img src="{{ asset('images/banner.jpg') }}" class="logo" alt="{{ config('app.name') }}" style="height: 60px;">
         </a>
     </td>
 </tr>
