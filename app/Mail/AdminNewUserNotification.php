6f6cbe2a05a0cba40afd3b1a917a7af69bfbf892
Epicpp <peverelli.t@gmail.com>
2024-10-14 18:24:00 +0200

    Update AdminNewUserNotification.php

--- app/Mail/AdminNewUserNotification.php
+++ app/Mail/AdminNewUserNotification.php
@@ -8,7 +8,6 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Models\User;
 
 class AdminNewUserNotification extends Mailable implements ShouldQueue
