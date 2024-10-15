deafe78d298667220a92852ee2c04f14b06434b1
Epicpp <peverelli.t@gmail.com>
2024-10-15 20:42:46 +0200

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,28 +1,25 @@
+// resources/js/bootstrap.js
+
 import axios from 'axios';
-import _ from 'lodash'; // Assurez-vous que lodash est installé ou commentez cette ligne si non utilisé
-
-window._ = _;
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
-window.Pusher = Pusher;
+// Initialiser Axios
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // Initialiser Laravel Echo
+window.Pusher = Pusher;
+
+// Ajoutez un log pour vérifier les valeurs des variables d'environnement
+console.log('Pusher Key:', import.meta.env.VITE_PUSHER_APP_KEY);
+console.log('Pusher Cluster:', import.meta.env.VITE_PUSHER_APP_CLUSTER);
+
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true,
+    encrypted: true,
+    // logToConsole: true, // Décommentez pour le débogage
 });
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allow your team to quickly build robust real-time web applications.
- */
-
-import './echo';
