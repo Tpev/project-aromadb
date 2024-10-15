4084a3ba3dad3e7fceb68e30c457ace980e87fa0
Epicpp <peverelli.t@gmail.com>
2024-10-15 13:16:30 +0200

    Update daily_kpi.blade.php

--- resources/views/emails/daily_kpi.blade.php
+++ resources/views/emails/daily_kpi.blade.php
@@ -5,12 +5,12 @@ Bonjour les Lapiz,
 
 {{-- Afficher un message festif si un nouveau record est atteint --}}
 @if($kpis['isNewHigh'])
-	@component('mail::message1')
+
 
 	 ![Bravo](https://aromamade.com/images/congratz.webp)
      🎉 Nouveau Record Atteint ! 🎉
         Hier, vous avez atteint un nouveau record de **{{ $kpis['sessionsYesterday'] }}** sessions, dépassant l'ancien record de **{{ $kpis['lastHighCount'] }}** sessions (Le {{ \Carbon\Carbon::parse($kpis['lastHighDate'])->format('d/m/Y') }}).
-@endcomponent
+
 @endif
 
 Voici les KPIs pour le **{{ \Carbon\Carbon::now()->format('d/m/Y') }}** :
