c329e84c58ec3ba8cfb79e4a9785b82514782f01
Epicpp <peverelli.t@gmail.com>
2024-10-29 11:24:49 +0100

    Update TestCertificateController.php

--- app/Http/Controllers/TestCertificateController.php
+++ app/Http/Controllers/TestCertificateController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Facades\Image;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Typography\FontFactory;
 
@@ -11,27 +10,30 @@ class TestCertificateController extends Controller
 {
     public function generateTestCertificate()
     {
-        // Load the base certificate image
-        $image = Image::make(public_path('images/certificat.png'));
 
-        // Write text on the image
-        $image->text('The quick brown fox', 120, 100, function ($font) {
-            $font->file(public_path('images/Roboto-Regular.ttf'));
-            $font->size(70);
-            $font->color('#FFFFFF');
-            $font->align('center');
-            $font->valign('middle');
-            $font->angle(10);
-        });
 
-        // Define the path to save the generated image
+
+// create test image
+$image = ImageManager::imagick()->read('images/certificat.png');
+
+// write text to image
+$image->text('The quick brown fox', 120, 100, function (FontFactory $font) {
+    $font->filename('images/Roboto-Regular.ttf');
+    $font->size(70);
+    $font->color('fff');
+    $font->stroke('ff5500', 2);
+    $font->align('center');
+    $font->valign('middle');
+    $font->lineHeight(1.6);
+    $font->angle(10);
+    $font->wrap(250);
+});
+// Define the path to save the generated image
         $filename = 'certificates/generated_certificate.png';
         $path = public_path($filename);
 
         // Save the image to the public directory
-        $image->save($path);
-
-        // Pass the relative path to the view
-        return view('test.certificate', ['imagePath' => $filename]);
+        $image->save($path); 
+        return view('test.certificate', compact('image'));
     }
 }
