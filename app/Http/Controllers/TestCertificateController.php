32fb4b0fb34201daa47777101e8ef7cdd8a0ebaa
Epicpp <peverelli.t@gmail.com>
2024-10-29 11:07:13 +0100

    update

--- app/Http/Controllers/TestCertificateController.php
+++ app/Http/Controllers/TestCertificateController.php
@@ -4,42 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\ImageManager;
+use Intervention\Image\Typography\FontFactory;
 
 class TestCertificateController extends Controller
 {
     public function generateTestCertificate()
     {
-        // Instantiate ImageManager with the 'gd' driver explicitly
-        $manager = new ImageManager('gd');
 
-        // Path to the template certificate image
-        $certificatePath = public_path('images/certificat_template.png');
 
-        // Read image from the filesystem
-        $image = $manager->make($certificatePath);
 
-        // Static fullff name for testing
-        $fullName = 'Jean Dupont';
+// create test image
+$image = ImageManager::imagick()->read('images/certificat.png');
 
-        // Path to font file (ensure it exists in the specified location)
-        $fontPath = public_path('fonts/Roboto-Regular.ttf');
-
-        // Add static text to the certificate
-        $image->text($fullName, 335, 270, function ($font) use ($fontPath) {
-            $font->file($fontPath);
-            $font->size(40); // Font size
-            $font->color('#5E6A0D'); // Text color to match the design
-            $font->align('center');
-            $font->valign('middle');
-        });
-
-        // Save the modified certificate image
-        $fileName = 'test_certificate.png';
-        $filePath = 'public/certificates/' . $fileName;
-        Storage::put($filePath, (string) $image->encode('png'));
-
-        // Get the URL for the saved certificate
-        $certificateUrl = Storage::url($filePath);
+// write text to image
+$image->text('The quick brown fox', 120, 100, function (FontFactory $font) {
+    $font->filename('images/Roboto-Regular.ttf');
+    $font->size(70);
+    $font->color('fff');
+    $font->stroke('ff5500', 2);
+    $font->align('center');
+    $font->valign('middle');
+    $font->lineHeight(1.6);
+    $font->angle(10);
+    $font->wrap(250);
+});
         
         return view('test.certificate', compact('certificateUrl'));
     }
