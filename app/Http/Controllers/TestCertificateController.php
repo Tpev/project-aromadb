bb6ebd7310dcc0e52653c0e6441c2776a168c4f1
Epicpp <peverelli.t@gmail.com>
2024-10-29 11:22:46 +0100

    Update TestCertificateController.php

--- app/Http/Controllers/TestCertificateController.php
+++ app/Http/Controllers/TestCertificateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Typography\FontFactory;
 
@@ -10,25 +11,27 @@ class TestCertificateController extends Controller
 {
     public function generateTestCertificate()
     {
+        // Load the base certificate image
+        $image = Image::make(public_path('images/certificat.png'));
 
+        // Write text on the image
+        $image->text('The quick brown fox', 120, 100, function ($font) {
+            $font->file(public_path('images/Roboto-Regular.ttf'));
+            $font->size(70);
+            $font->color('#FFFFFF');
+            $font->align('center');
+            $font->valign('middle');
+            $font->angle(10);
+        });
 
+        // Define the path to save the generated image
+        $filename = 'certificates/generated_certificate.png';
+        $path = public_path($filename);
 
-// create test image
-$image = ImageManager::imagick()->read('images/certificat.png');
+        // Save the image to the public directory
+        $image->save($path);
 
-// write text to image
-$image->text('The quick brown fox', 120, 100, function (FontFactory $font) {
-    $font->filename('images/Roboto-Regular.ttf');
-    $font->size(70);
-    $font->color('fff');
-    $font->stroke('ff5500', 2);
-    $font->align('center');
-    $font->valign('middle');
-    $font->lineHeight(1.6);
-    $font->angle(10);
-    $font->wrap(250);
-});
-        
-        return view('test.certificate', compact('image'));
+        // Pass the relative path to the view
+        return view('test.certificate', ['imagePath' => $filename]);
     }
 }
