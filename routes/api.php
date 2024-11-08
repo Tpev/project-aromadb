0d0d14a93ecfb323afabe92248c0c66a44e318e9
Epicpp <peverelli.t@gmail.com>
2024-11-08 14:46:26 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,13 +6,10 @@ use App\Events\SignalingEvent;
 use App\Http\Controllers\SignalingController;
 use App\Http\Controllers\StripeWebhookController;
 
-
 Route::post('/subscription', [StripeWebhookController::class, 'handleWebhook']);
-
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/signaling', [SignalingController::class, 'signaling']);
-
