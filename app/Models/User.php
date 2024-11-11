54b995a63aac9c7a841cd0351e8b285cf69b5f4b
Epicpp <peverelli.t@gmail.com>
2024-11-11 11:50:36 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,6 +60,10 @@ class User extends Authenticatable
     public function favorites()
     {
         return $this->hasMany(Favorite::class);
+    }    
+	public function events()
+    {
+        return $this->hasMany(Event::class);
     }
 
     public function products()
