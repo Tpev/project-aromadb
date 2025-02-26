62e675c724ef5649d6082e637de3c88af99cfe07
Epicpp <peverelli.t@gmail.com>
2025-02-26 18:14:17 +0100

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
+Route::middleware('guest',\App\Http\Middleware\TrackPageViews::class)->group(function () {
     // Normal User Register
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
@@ -42,7 +42,7 @@ Route::middleware('guest')->group(function () {
                 ->name('password.store');
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth',\App\Http\Middleware\TrackPageViews::class)->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
                 ->name('verification.notice');
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
