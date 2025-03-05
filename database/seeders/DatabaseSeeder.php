3398c3398a716a8e2e97f2cf87170088ad4dffc9
Epicpp <peverelli.t@gmail.com>
2025-03-05 12:56:21 +0100

    fix training

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            TrainingSeeder::class,
-            ChapterSeeder::class,
-            LessonSeeder::class,
-        ]);
+
     }
 }
