f45fd2c5fc438e652759b0a84f18095679306243
Epicpp <peverelli.t@gmail.com>
2025-06-25 13:01:21 +0200

    Update appointment_reminder.blade.php

--- resources/views/emails/appointment_reminder.blade.php
+++ resources/views/emails/appointment_reminder.blade.php
@@ -1,4 +1,11 @@
 <!-- resources/views/emails/appointment_reminder.blade.php -->
+@php
+    // Chaîne lisible prête à être affichée (ex.: "En Visio, Dans le Cabinet")
+    $modes = $appointment->product->getConsultationModes();
+
+    // Vérifie si la chaîne contient exactement "Dans le Cabinet"
+    $showsCabinetAddress = str_contains($modes, 'Dans le Cabinet');
+@endphp
 
 @component('mail::message')
 # Bonjour {{ $appointment->clientProfile->first_name }},
@@ -10,6 +17,12 @@ Ceci est un rappel pour votre rendez-vous prévu le {{ $appointment->appointment
 - **Date et heure :** {{ $appointment->appointment_date->format('d/m/Y à H:i') }}
 - **Durée :** {{ $appointment->duration }} minutes
 - **Prestation :** {{ $appointment->product->name }}
+- **Mode de consultation :** {{ $modes }}
+
+@if ($showsCabinetAddress && $appointment->user?->company_address)
+**Adresse du cabinet :**  
+{{ $appointment->user->company_address }}
+@endif
 - **Thérapeute :** {{ $appointment->user->name }}
 
 Si vous avez des questions, n'hésitez pas à nous contacter.
