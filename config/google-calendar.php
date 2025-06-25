60de1cc072a2d6b4d08b93991e14882404a2ad16
Epicpp <peverelli.t@gmail.com>
2025-06-25 17:16:45 +0200

    Update google-calendar.php

--- config/google-calendar.php
+++ config/google-calendar.php
@@ -2,23 +2,15 @@
 
 return [
 
-    // Utiliser uniquement OAuth
-    // Nouveau code (forcé)
-'default_auth_profile' => 'oauth',
-
+    'default_auth_profile' => 'oauth',
 
 'auth_profiles' => [
     'oauth' => [
-        // ← pointez sur le fichier que vous venez de copier
         'credentials_json' => storage_path('app/google-calendar/oauth-credentials.json'),
-        'token_json'       => null,   // on n’a toujours pas besoin de fichier token
+        'token_json'       => storage_path('app/google-calendar/oauth-token.json'), // ← plus NULL
     ],
 ],
 
-
-    // Agenda par défaut
     'calendar_id'        => env('GOOGLE_CALENDAR_ID', 'primary'),
-
-    // Impersonation (laissez vide)
     'user_to_impersonate'=> env('GOOGLE_CALENDAR_IMPERSONATE'),
 ];
