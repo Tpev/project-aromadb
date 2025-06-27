ba50480897b9b59bf79319d74b7ad27cb14c8a0d
Epicpp <peverelli.t@gmail.com>
2025-06-27 20:27:21 +0200

    improve

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -7,7 +7,7 @@
      | 1. logo.png must be square (or you can set different height/width)
      | 2. if you have a @2x / Retina asset, add it to /images/logo@2x.png
      | -----------------------------------------------------------*/
-    $logo1x = asset('images/png-01.png');          // 128 × 128 px
+    $logo1x = asset('images/png-01.webp');          // 128 × 128 px
     $logo2x = asset('images/png-01@2x.png');       // 256 × 256 px (optional)
 @endphp
 
