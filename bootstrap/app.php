045dc9043b845e79fc6e946af2c253e2be0928f3
Epicpp <peverelli.t@gmail.com>
2025-11-26 12:47:54 +0100

    up with middleware for mobile

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // 👇 Add your custom middleware alias here.
+        // This does NOT remove Laravel’s default aliases/groups.
+        $middleware->alias([
+            'mobile.app' => \App\Http\Middleware\EnsureMobileApp::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
--- routes/mobile.php
+++ routes/mobile.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web'])->group(function () {
+Route::middleware(['web', 'mobile.app'])->group(function () {
     Route::get('/mobile', function () {
         return view('mobile.entry');
     })->name('mobile.entry');
 });
-
