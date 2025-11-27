7d40d2559b12644b7bf07c12b876a6a19292280f
Epicpp <peverelli.t@gmail.com>
2025-11-27 10:09:48 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/mobile.js',
-                'resources/css/mobile.css',
+                'resources/js/mobile.js', 
+                'resources/css/mobile.css', 
+            ],
             refresh: true,
         }),
     ],
