cde963d3b5c83325e4436202ba8b6f03c042ce29
Epicpp <peverelli.t@gmail.com>
2025-11-27 10:06:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-
-
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/mobile.js',
+                'resources/css/mobile.css',
             refresh: true,
         }),
     ],
     resolve: {
         alias: {
             '@': '/resources/js',
-
         },
     },
 });
