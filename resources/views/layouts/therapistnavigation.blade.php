152afe82f328fba7b5fbb3a080202b69d371e0ea
Epicpp <peverelli.t@gmail.com>
2026-04-09 12:37:34 +0200

    Update therapistnavigation.blade.php

--- resources/views/layouts/therapistnavigation.blade.php
+++ resources/views/layouts/therapistnavigation.blade.php
@@ -179,9 +179,6 @@
                             </a>
                             <a href="{{ $giftVoucherNavUrl }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                                 {{ __('Bon cadeau') }}
-                                @unless($canUseGiftVouchers)
-                                    <span class="ml-1 text-xs font-semibold text-amber-700">(Upgrade)</span>
-                                @endunless
                             </a>
                             <a href="{{ route('questionnaires.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                                 {{ __('Questionnaires') }}
@@ -374,7 +371,7 @@
                 {{ __('Evénements') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="$giftVoucherNavUrl" :active="request()->routeIs('pro.gift-vouchers.*')" class="text-[#647a0b] hover:text-[#854f38]">
-                {{ __('Bon cadeau') }} @unless($canUseGiftVouchers) {{ __('(Upgrade)') }} @endunless
+                {{ __('Bon cadeau') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('availabilities.index')" :active="request()->routeIs('availabilities.*')" class="text-[#647a0b] hover:text-[#854f38]">
                 {{ __('Disponibilités') }}
